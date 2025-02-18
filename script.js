a4806e70c66509f9f8e17f0dbf2b73c796cbff52
danidani1907 <danielli.freitas1907@gmail.com>
2025-02-18 16:34:12 -0300

    parte 2 reafzendo

---
+++ script.js
@@ -0,0 +1,31 @@
+let lista = document.querySelectorAll('.item')
+
+let next = document.getElementById('proximo')
+let prev = document.getElementById('anterior')
+
+let contar = lista.length
+let ativo = 0
+
+next.onclick=()=>{
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    if(ativo>=contar-1){
+        ativo=0;
+    }else{
+        ativo=ativo+1
+    }
+    lista[ativo].classList.add('ativo');
+}
+
+prev.onclick = () => {
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    if (ativo <= 0) {
+        ativo = contar - 1; 
+    } else {
+        ativo = ativo - 1;
+    }
+    lista[ativo].classList.add('ativo');
+}
+
+
